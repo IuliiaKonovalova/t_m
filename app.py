83d568687e9557864f8c6cc6f78eea8f316f48e6
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-29 14:22:44 +0800

    Add email requirements to register

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def get_tasks():
     return render_template("tasks.html", tasks=tasks)
 
 
+
 @app.route("/search", methods=["GET", "POST"])
 def search():
     query = request.form.get("query")
@@ -162,7 +163,6 @@ def add_category():
               {"category_name": request.form.get("category_name")}
           )
         if existing_category:
-            # ensure hashed password matches user input
             flash("Category already exist")
             return redirect(url_for("get_categories"))
         categories = {
@@ -192,7 +192,7 @@ def edit_category(category_id):
 def delete_category(category_id):
     mongo.db.categories.delete_one({"_id": ObjectId(category_id)})
     flash("Category Successfully Deleted")
-    return redirect(url_for("get_categories"))
+    return redirect(url_for("get_tasks"))
 
 
 if __name__ == "__main__":
